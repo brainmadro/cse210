28c0b7bfca0b4f038ce60cd83d89cb0c02f9054a
Brian Madroñero <brainmadro@gmail.com>
2024-05-01 11:15:04 -0500

    prep: conditionals

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter;
+        
+        Console.Write("Please enter your grade percentage: ");
+        float gradePercentage = float.Parse(Console.ReadLine());
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations!!");
+        }
+        else
+        {
+            Console.WriteLine("You can do it better next time");
+        }
     }
 }
