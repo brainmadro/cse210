222b272deb66d192d236116d020af32b84766581
Brian Madroñero <brainmadro@gmail.com>
2024-05-01 19:55:41 -0500

    prep: Resolve activity nnumber 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int response, sum = 0, largest = 0;
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        do
+        {
+            Console.Write("Enter a number: ");
+            response = int.Parse(Console.ReadLine());
+            numbers.Add(response);
+        } while (response != 0);
+        
+        foreach (var item in numbers)
+        {
+            sum += item;
+            if (item > largest)
+            {
+                largest = item;
+            }
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {sum / (numbers.Count - 1)}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
