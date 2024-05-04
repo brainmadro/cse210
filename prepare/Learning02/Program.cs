6f498ee3fec9c4d5dc7041ed21a963ec245edaba
Brian Madroñero <brainmadro@gmail.com>
2024-05-03 21:04:27 -0500

    feat: Abstreaction Learing Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        Job job2 = new Job();        
+        Resume resume = new Resume();
+
+        job1._company = "Microsoft";
+        job1._endYear = 2019;
+        job1._jobTitle = "Software Engeneer";
+        job1._startYear = 2022;
+        job2._company = "Apple";
+        job2._endYear = 2022;
+        job2._jobTitle = "Manager";
+        job2._startYear = 2023;
+
+        resume._name = "Brian Madroñero";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
     }
 }
