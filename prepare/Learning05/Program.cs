c9b18fdddfc702f8d4f5eec3373a31b0d5304fcc
Brian Madroñero <brainmadro@gmail.com>
2024-05-27 21:21:51 -0500

    feat Learning 05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape> {
+            new Circle("Red", 2),
+            new Rectangle("Green", 3, 4),
+            new Square("Blue", 5)
+        };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"The shape has an area of {shape.GetArea()} and is color {shape.GetColor()}.");
+        }
+
     }
 }
